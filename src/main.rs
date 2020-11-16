8c55d6a4f7e8871b8874e21a65dcf96952e21d58
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-16 14:30:07 -0700

    using mem to show data types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
 
+use std::mem;
+
 fn main() {
     let a: u8 = 128; // u = unsigned, 8 bits, 0 - 255
     println!("a={}", a); // immutable
@@ -11,4 +13,14 @@ fn main() {
     println!("b = {} before", b);
     b = 42;
     println!("b = {} after", b);
+
+    let mut c = 123456789; // i32 = 32 bits = 4 bytes
+    println! ("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
+    c = -1;
+    println!("c = {}", c);
+
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!("z = {}, takes up {} bytes, {}=bit OS",
+        z, size_of_z, size_of_z*8);
 }
