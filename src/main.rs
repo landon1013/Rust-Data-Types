6d41264695f8232b5cbb2e7c1f9ec41b7f70e3de
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-16 14:19:49 -0700

    Immutable and Mutable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let a: u8 = 128; // u = unsigned, 8 bits, 0 - 255
+    println!("a={}", a); // immutable
+
+        // u = unsigned, 0 to 2^N-1
+        // i = signed, -2^(N-1) .. 2^(N-1)-1
+    let mut b: i8 = 0; // -128 -- 127
+    println!("b = {} before", b);
+    b = 42;
+    println!("b = {} after", b);
 }
